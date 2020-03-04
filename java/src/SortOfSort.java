44d961fe0719dd8705740faeef471c81752e9711
Caortega4 <53986225+Caortega4@users.noreply.github.com>
2020-03-04 12:06:27 -0700

    Finished

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,6 +1,7 @@
 public class SortOfSort {
     public static void sortOfSort(int[] nums){
 
+        //If the input array is size 1 or 0 that means it is already sorted so it does not changes anything
         if(nums.length < 2)
             return ;
 
@@ -8,45 +9,44 @@ public class SortOfSort {
         int temp = Integer.MIN_VALUE;
         int leftOffset = 0;
         int rightOffset = 0;
-        int count1 = 0;
-        int count2 = 0;
+        int countRight = 0;
+        int countLeft = 0;
 
+        //There will be n swaps therefore a loop to execute the inner loop that amount of times
         for (int i = 0; i < nums.length; i++) {
+            //It will not check a number that is already sorted so it will start checking after that
             highestIndex = leftOffset;
+            //In order to not check indices that are already sorted it will start after and end before the corresponding offsets
             for (int j = leftOffset; j < nums.length - rightOffset; j++) {
+                //Checks for the highest unsorted value in the array
                 if(nums[j] > nums[highestIndex]) {
                     highestIndex = j;
                 }
             }
-            if (count1 < 2 ) {
-                count2 = 0;
+            //When it has sorted two elements at the right it will switch to add at the left
+            //Each branch swaps the highest unsorted element with the one on its corresponding position
+            if (countRight < 2 ) {
+                countLeft = 0;
                 temp = nums[nums.length - 1 - rightOffset] ;
                 nums[nums.length - 1 - rightOffset] = nums[highestIndex];
                 nums[highestIndex] = temp;
                 rightOffset++;
-                count1++;
+                countRight++;
             }else {
 
                 temp = nums[leftOffset];
                 nums[leftOffset] = nums[highestIndex];
                 nums[highestIndex] = temp;
                 leftOffset++;
-                count2++;
-                if (count2 == 2){
-                    count1 = 0;
+                countLeft++;
+                //If it has sorted two elements at the left it rests the count of the right to start adding there again
+                if (countLeft == 2){
+                    countRight = 0;
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8};
-        sortOfSort(arr);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-        }
-
-    }
 
 }
--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 public class SortOfSortTester {
     @Test
 
+    //Test case for an array of even size and already sorted on increasing order in order to test minimum requirements
     public void testSortOfSort1(){
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
         SortOfSort.sortOfSort(arr);
@@ -17,6 +18,7 @@ public class SortOfSortTester {
 
     @Test
 
+    //Test case for an even size and unordered content
     public void testSortOfSort2(){
         int[] arr = {8,5,6,4,2,7,1,3};
         SortOfSort.sortOfSort(arr);
@@ -26,15 +28,17 @@ public class SortOfSortTester {
 
     @Test
 
+    //Test case for an empty array to test how the methods handles this particular input
     public void testSortOfSort3(){
-        int[] arr = {5};
+        int[] arr = {};
         SortOfSort.sortOfSort(arr);
-        int[] expected = {5};
+        int[] expected = {};
         Assert.assertArrayEquals(expected, arr);
     }
 
     @Test
 
+    //Test case with an odd size where the lowest element sorted at the right half of the array
     public void testSortOfSort4(){
         int[] arr = {6,4,2,8,5};
         SortOfSort.sortOfSort(arr);
@@ -44,6 +48,7 @@ public class SortOfSortTester {
 
     @Test
 
+    //Test case with an odd size where the lowest element sorted at the left half of the array
     public void testSortOfSort5(){
         int[] arr = {5,3,7,55,88,9,66};
         SortOfSort.sortOfSort(arr);
